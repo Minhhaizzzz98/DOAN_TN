36552db124e57d02004b39164bef364a529da933
BaoNguyen01121995 <baonguyen01121995@gmail.com>
2021-11-15 22:27:56 +0700

    [MA] Implemented importing list SinhVien

---
+++ Admin/Admin/Models/CsvImporter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin.Models
+{
+    public class CsvImporter
+    {
+        public int MaSV { get; set; }
+        public string TenSV { get; set; }
+        public string DiaChi { get; set; }
+        public string SoDienThoai { get; set; }
+        public string Email { get; set; }
+        public bool TrangThai { get; set; }
+        public int Lop { get; set; }
+        public int MaTaiKhoan { get; set; }
+
+        public IFormFile FileUpload { set; get; }
+
+    }
+}
